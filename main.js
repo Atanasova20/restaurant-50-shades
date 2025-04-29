deb628adf1b7203b5fbb7cae304b45dc70d74a37
Atanasova20 <Vanyaatanasova1111@gmail.com>
2025-04-29 14:37:48 +0300

    Update

---
+++ main.js
@@ -0,0 +1,42 @@
+// прихващане на класовете за промяна на хедъра при скролване
+window.addEventListener('scroll', function () {
+  let header = document.getElementById('header');
+  if (window.scrollY > 50) {
+    header.classList.add('bg-[#0e0f10]', 'shadow-md');
+    header.classList.remove('bg-transparent');
+  } else {
+    header.classList.remove('bg-[#0e0f10]', 'shadow-md');
+    header.classList.add('bg-transparent');
+  }
+});
+
+// формата за резервация (падащите менюта)
+
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownMenu = document.getElementById('dropdownMenu');
+const selectedText = document.getElementById('selectedText');
+const dropdownIcon = document.getElementById('dropdownIcon');
+
+// Toggle dropdown visibility
+dropdownButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('hidden');
+  dropdownIcon.classList.toggle('rotate-180');
+});
+
+// Set selected time and close dropdown
+const items = document.querySelectorAll('.dropdown-item');
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    selectedText.textContent = e.target.textContent;
+    dropdownMenu.classList.add('hidden');
+    dropdownIcon.classList.remove('rotate-180');
+  });
+});
+
+// Close dropdown when clicking outside
+document.addEventListener('click', (e) => {
+  if (!dropdownButton.contains(e.target) && !dropdownMenu.contains(e.target)) {
+    dropdownMenu.classList.add('hidden');
+    dropdownIcon.classList.remove('rotate-180');
+  }
+});
